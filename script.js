137262643875c9a40df3bb33abe8b93a68b15e91
Exotic P <sjwiegman@gmail.com>
2022-08-18 17:26:00 -0400

    tweaked small JavaScript errors, still broken

--- script.js
+++ script.js
@@ -6,29 +6,29 @@ function generatePassword() {
   var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var numbers = '0123456789';
   var special = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
+  
   var possible = '';
   var password = '';
 
   var character = 8;
 
-  var hasUpper = confirm("Include uppsercase letters?");
+  var hasUpper = confirm("Include uppercase letters?");
   var hasLower = confirm("Include lowercase letters?");
   var hasNumber = confirm("Include numbers?");
   var hasSpecial = confirm("Include special characters?");
 
-  if(hasUpper) {
+  if (hasUpper) {
     possible += upper;
   }
-  if(hasLower) {
+  if (hasLower) {
     possible += lower;
   }
-  if(hasNumber) {
+  if (hasNumber) {
     possible += numbers;
   }
-  if(hasSpecial) {
+  if (hasSpecial) {
     possible += special;
   }
-  
 
   for (var i = 0; i < character; i++) {
     var random = Math.floor(Math.random() * possible.length);
@@ -36,7 +36,7 @@ function generatePassword() {
     password += possible[random];
   }
 
-  return '';
+  console.log (password);
 }
 
 // Write password to the #password input
@@ -44,7 +44,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
